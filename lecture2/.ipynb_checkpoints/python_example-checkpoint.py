da6230a58800bcff25a33ef18c07c764a6c477b4
Juan Estrada <jjestra@emory.edu>
2022-08-29 14:32:57 -0400

    Add lecture 2 material

---
+++ lecture2/.ipynb_checkpoints/python_example-checkpoint.py
@@ -0,0 +1,5 @@
+print('Welcome! This is a program that adds two numbers')
+
+n1 = int(input('Enter the first number: '))
+n2 = int(input('Enter the second number: '))
+print('The sum of the two numbers is', n1+n2)
